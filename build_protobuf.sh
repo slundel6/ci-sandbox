aac287ccee15be0a2bf9b1b6168f18db71e0f217
slundel6 <slundel6@volvocars.com>
2026-05-05 13:48:43 +0200

    Updated script

--- build_protobuf.sh
+++ build_protobuf.sh
@@ -3,7 +3,7 @@
 BUILD_JOBS=2
 BUILD_PARALLEL_ARGS=(--parallel "${BUILD_JOBS}")
 
-if [ "$OSTYPE" == "msys" ]; then
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     # Visual Studio 2022 using toolkit from Visual Studio 2017
     GENERATOR=("Visual Studio 17 2022")
     GENERATOR_TOOLSET="v142"
@@ -52,11 +52,16 @@ cmake -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -S . -B build \
 	-DABSL_PROPAGATE_CXX_STD=ON
 
 cmake --build build --config Release "${BUILD_PARALLEL_ARGS[@]}"
-cmake --install build --prefix dist
+cmake --install build --prefix install
 cd ..
 
-ZLIB_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/zlib/install")
-ABSL_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/abseil-cpp/dist")
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    ZLIB_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/zlib/install")
+    ABSL_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/abseil-cpp/install")
+else
+    ZLIB_PATH="${ROOT_DIR}/osi-dependencies/zlib/install"
+    ABSL_PATH="${ROOT_DIR}/osi-dependencies/abseil-cpp/install"
+fi
 
 # build and install protobuf
 git clone --depth 1 --branch v29.3 https://github.com/protocolbuffers/protobuf.git
@@ -75,19 +80,34 @@ cmake --install cmake-out --prefix install
 cd $ROOT_DIR
 
 # build and install osi
-git clone --recurse-submodules https://github.com/slundel6/osi-cpp.git
+git clone --depth 1 --branch main --recurse-submodules https://github.com/slundel6/osi-cpp.git
 mkdir osi-cpp-install
 cd osi-cpp
 mkdir build
 
-PROTO_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/protobuf/install")
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    PROTO_PATH=$(cygpath -m "${ROOT_DIR}/osi-dependencies/protobuf/install")
+else
+    PROTO_PATH="${ROOT_DIR}/osi-dependencies/protobuf/install"
+fi
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    OSI_LIB_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/lib")
+    OSI_INCLUDE_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/include")
+    OSI_CMAKE_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/cmake")
+else
+    OSI_LIB_DIR="${ROOT_DIR}/osi-cpp-install/lib"
+    OSI_INCLUDE_DIR="${ROOT_DIR}/osi-cpp-install/include"
+    OSI_CMAKE_DIR="${ROOT_DIR}/osi-cpp-install/cmake"
+fi
 
 cmake -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -S . -B build \
     "-DCMAKE_CXX_STANDARD=17" \
+    "-DCMAKE_BUILD_TYPE=Release" \
     "-DCMAKE_PREFIX_PATH=${PROTO_PATH};${ABSL_PATH};${ZLIB_PATH}" \
-    "-DOSI_INSTALL_LIB_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/lib")" \
-    "-DOSI_INSTALL_INCLUDE_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/include")" \
-    "-DOSI_INSTALL_CMAKE_DIR=$(cygpath -m "${ROOT_DIR}/osi-cpp-install/cmake")"
+    "-DOSI_INSTALL_LIB_DIR=${OSI_LIB_DIR}" \
+    "-DOSI_INSTALL_INCLUDE_DIR=${OSI_INCLUDE_DIR}" \
+    "-DOSI_INSTALL_CMAKE_DIR=${OSI_CMAKE_DIR}"
 
 cmake --build build --config Release --clean-first "${BUILD_PARALLEL_ARGS[@]}"
-cmake --install build
+cmake --install build --config Release
